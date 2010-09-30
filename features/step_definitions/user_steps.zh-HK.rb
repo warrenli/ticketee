fdbf08408e5129ae0317864a9c3136d6ae0340d1
Warren Li <warrenli@dontrush.org>
2010-09-30 10:00:08 +0800

    Added feature for signing in via the Devise-provided form

--- features/step_definitions/user_steps.zh-HK.rb
+++ features/step_definitions/user_steps.zh-HK.rb
@@ -3,3 +3,7 @@
 假設 /^有一位帳戶電郵地址是"([^"]*)"密碼是"([^"]*)"$/ do |email, password|
   User.create!(:email => email, :password => password, :password_confirmation => password)
 end
+
+假設 /^"([^"]*)"已確認帳戶$/ do |email|
+  User.find_by_email(email).confirm!
+end
