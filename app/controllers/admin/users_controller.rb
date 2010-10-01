187baf1408c40e94bd55e63f7543585fc38a9222
Warren Li <warrenli@dontrush.org>
2010-10-01 11:23:33 +0800

    Added the ability to edit users through the admin backend

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,4 +1,6 @@
 class Admin::UsersController < Admin::BaseController
+  before_filter :find_user, :only => [:show, :edit, :update, :destroy]
+
   def index
     @users = User.all(:order => "email")
   end
@@ -9,7 +11,7 @@ class Admin::UsersController < Admin::BaseController
 
   def create
     @user = User.new(params[:user])
-    @user.admin = params[:user][:admin] == "1"
+    set_admin
     if @user.save
       flash[:notice] = t("admin_user.created_msg")
       redirect_to admin_users_path
@@ -17,5 +19,36 @@ class Admin::UsersController < Admin::BaseController
       flash[:alert] = t("admin_user.not_created_msg")
       render :action => "new"
     end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if params[:user][:password].blank?
+      params[:user].delete(:password)
+      params[:user].delete(:password_confirmation)
+    end
+    set_admin
+    if @user.update_attributes(params[:user])
+      flash[:notice] = t("admin_user.updated_msg")
+      redirect_to admin_users_path
+    else
+      flash[:alert] = t("admin_user.not_updated_msg")
+      render :action => "edit"
+    end
+  end
+
+  private
+
+  def find_user
+    @user = User.find(params[:id])
+  end
+
+  def set_admin
+    @user.admin = params[:user][:admin] == "1"
   end
 end
